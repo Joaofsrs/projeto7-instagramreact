950c8f524937d65b354eb8d5ea5127e48cc45274
Joaofsrs <rsjoao12@gmail.com>
2023-02-11 03:18:05 -0300

    Dados dinâmicos cmo like e nome do perfil

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,36 @@
+import React from 'react'
+
 export default function Post(prop) {
+    const [salvo, setSalvo] = React.useState("bookmark-outline");
+    const [like, setLike] = React.useState("heart-outline");
+    const [numLike, setNumLike] = React.useState(prop.post.numeroCurtidas);
+
+    function salvarPost(){
+        if(salvo === "bookmark-outline"){
+            setSalvo("bookmark");
+        }else{
+            setSalvo("bookmark-outline");
+        }
+    }
+
+    function likePost(opcao){
+        let aux =  Number(numLike.replace(".", ""))
+        if(opcao === "coracao"){
+            if(like === "heart-outline"){
+                setLike("heart");
+                setNumLike((aux+1).toLocaleString('pt-BR'));
+            }else{
+                setLike("heart-outline");
+                setNumLike((aux-1).toLocaleString('pt-BR'));
+            }
+        }else if(opcao === "foto"){
+            if(like === "heart-outline"){
+                setLike("heart");
+                setNumLike((aux+1).toLocaleString('pt-BR'));
+            }
+        }
+    }
+
     return (
         <div class="post">
             <div class="topo">
@@ -12,25 +44,25 @@ export default function Post(prop) {
             </div>
 
             <div class="conteudo">
-                <img src={prop.post.imgPost} alt={prop.post.altPost} />
+                <img src={prop.post.imgPost} alt={prop.post.altPost} onClick={() => likePost("foto")} />
             </div>
 
             <div class="fundo">
                 <div class="acoes">
                     <div>
-                        <ion-icon name="heart-outline"></ion-icon>
+                        <ion-icon name={like} onClick={() => likePost("coracao")} class={(like === "heart") ? "vermelho" : ""} ></ion-icon>
                         <ion-icon name="chatbubble-outline"></ion-icon>
                         <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
                     <div>
-                        <ion-icon name="bookmark-outline"></ion-icon>
+                        <ion-icon name={salvo} onClick={salvarPost}></ion-icon>
                     </div>
                 </div>
 
                 <div class="curtidas">
                     <img src={prop.post.imgCurtida} alt="respondeai" />
                     <div class="texto">
-                        Curtido por <strong>{prop.postusuarioCurtida}</strong> e <strong>outras {prop.post.numeroCurtidas} pessoas</strong>
+                        Curtido por <strong>{prop.postusuarioCurtida}</strong> e <strong>outras {numLike} pessoas</strong>
                     </div>
                 </div>
             </div>
--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,11 +1,30 @@
-export default function Usuario() {
+import React from 'react'
+
+export default function Usuario(){
+    const [nomeUsuario, setNome] = React.useState("catanacomics");
+    const [fotoUsuario, setFoto] = React.useState("assets/img/catanacomics.svg");
+
+    function mudaNome(){
+        const novoNome = prompt("Digite um novo nome");
+        if(novoNome !== ""){
+            setNome(novoNome);
+        }
+    }
+
+    function mudaFoto(){
+        const novaFoto = prompt("Digite a URL da foto");
+        if(novaFoto !== ""){
+            setFoto(novaFoto);
+        }
+    }
+
     return (
         <div class="usuario">
-            <img src="assets/img/catanacomics.svg" alt="imagem de perfil" />
+            <img src={fotoUsuario} alt="imagem de perfil" onClick={mudaFoto} />
             <div class="texto">
                 <span>
-                    <strong>catanacomics</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong>{nomeUsuario}</strong>
+                    <ion-icon name="pencil" onClick={mudaNome}></ion-icon>
                 </span>
             </div>
         </div>
